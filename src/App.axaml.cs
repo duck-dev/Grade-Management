f374a310f531faf0fd571b41436a8dca7bc2634e
Duck <61479436+NiceDuck@users.noreply.github.com>
2021-10-01 14:04:06 +0200

    Initial commit
    - This is the initial commit containing all basic Avalonia (MVVM-pattern) files and directories with a few additional directories, based on my preference. The project is still pretty much empty though.

---
+++ src/App.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using Grade_Management.ViewModels;
+using Grade_Management.Views;
+
+namespace Grade_Management
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
