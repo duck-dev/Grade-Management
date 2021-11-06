b3fd21fb5eb23d303c61f4fcb5ed2c0d8448939c
Duck <61479436+duck-dev@users.noreply.github.com>
2021-11-06 22:35:31 +0100

    Subject-buttons have a nicer tint on hover
    When hovering over a button in the list of subjects, the background tint is not the light grey background color anymore, but a slightly darker tint of the main color of the subject.
    
    - Added `BackgroundBrush` property for the main linear gradient brush used as the normal background of a subject with the specified `SubjectColor` in the `Subject` class (`Subject.cs`)
    - Added `BackgroundBrushHover` property, which is just a darker tint of the main linear gradient for the background.
    - Added `CreateGradientBrush`, which is responsible for the logic of creating a Linear Gradient Brush. This might be moved to `Utilities` (`ColorUtilities`)
    - Removed comments in `Subject.cs`, because that's not a case where so many comments are necessarily needed. It's not a utility class and the exposed properties can and will probably be `private` in the end anyway, due to the reflection linking between the views and this model.

--- src/Models/Subject.cs
+++ src/Models/Subject.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Avalonia;
 using Avalonia.Media;
 using GradeManagement.Interfaces;
 using GradeManagement.UtilityCollection;
@@ -12,28 +13,13 @@ namespace GradeManagement.Models
         private readonly string _subjectColorHex;
         private readonly List<Grade> _grades;
         private bool _counts;
-        
-        private readonly Color _additionalInfoDark = Color.Parse("#7a7a7a");
-        private readonly Color _additionalInfoLight = Color.Parse("#b8b8b8");
 
-        /// <summary>
-        /// A constructor with no color being passed, which automatically sets a default gray-blue-ish shade by calling
-        /// another constructor with 4 parameters.
-        /// </summary>
-        /// <param name="name">The name of the subject.</param>
-        /// <param name="weighting">The weighting of the subject-average for the final year-average.</param>
-        /// <param name="grades">The collection of grades included in this subject.</param>
+        private readonly Color _additionalInfoColor = Color.Parse("#999999");
+        private readonly Color _lightBackground = Color.Parse("#c7cad1"); // TODO: Will probably be in a special class for all the colors and variations of them
+        
         public Subject(string name, float weighting, IEnumerable<Grade> grades) : this(name, weighting, "#c7cad1", 
         grades, true) {}
         
-        /// <summary>
-        /// A constructor with 4 parameters:
-        /// </summary>
-        /// <param name="name">The name of the subject.</param>
-        /// <param name="weighting">The weighting of the subject-average for the final year-average.</param>
-        /// <param name="color">The color of the subject, being used as the background color of the button, representing
-        ///                     this subject in the User Interface.</param>
-        /// <param name="grades">The collection of grades included in this subject.</param>
         public Subject(string name, float weighting, string color, IEnumerable<Grade> grades, bool counts)
         {
             _name = name;
@@ -43,87 +29,80 @@ namespace GradeManagement.Models
             _counts = counts;
         }
         
-        /// <summary>
-        /// The weighting of the subject-average for the final year-average.
-        /// </summary>
         public float Weighting => _weighting;
         
-        /// <summary>
-        /// The hex-representation of the subject-color. The color of this subject is being used as the background
-        /// color of the button representing this subject in the User Interface.
-        /// </summary>
         public string SubjectColorHex => _subjectColorHex;
-        
-        /// <summary>
-        /// The subject-color wrapped in the Avalonia.Media.Color struct. The color of this subject is being used
-        /// as the background color of the button representing this subject in the User Interface.
-        /// </summary>
         public Color SubjectColor => Color.Parse(_subjectColorHex);
-
-        /// <summary>
-        /// The color of the title, being a bit darker than the <see cref="SubjectColor"/>
-        /// if the background is bright and vice-versa.
-        /// </summary>
         public SolidColorBrush TitleBrush => 
             new(Utilities.AdjustForegroundBrightness(SubjectColor, DarkSubjectTint, LightSubjectTint));
 
-        /// <summary>
-        /// The color of the additional information in the User Interface (grades-count and weighting)
-        /// with adjusted brightness, based on the background color, being the <see cref="SubjectColor"/>
-        /// </summary>
+        public LinearGradientBrush BackgroundBrush
+        {
+            get
+            {
+                return CreateGradientBrush(new RelativePoint(0, 100, RelativeUnit.Relative),
+                    new RelativePoint(100, 0, RelativeUnit.Relative),
+                    new[] {SubjectColor, _lightBackground},
+                    new[] {0.0, 0.9});
+            }
+        }
+
+        public LinearGradientBrush BackgroundBrushHover
+        {
+            get
+            {
+                return CreateGradientBrush(new RelativePoint(0, 100, RelativeUnit.Relative),
+                    new RelativePoint(100, 0, RelativeUnit.Relative),
+                    new[] {Utilities.DarkenColor(SubjectColor, 0.1f), Utilities.DarkenColor(_lightBackground, 0.1f)},
+                    new[] {0.0, 0.9});
+            }
+        }
+        
         public SolidColorBrush AdditionalInfoColor => 
-            new(Utilities.AdjustForegroundBrightness(SubjectColor, _additionalInfoDark, _additionalInfoLight));
-
-        /// <summary>
-        /// The exact average of all grades in this subject (not rounded).
-        /// </summary>
+            new(Utilities.AdjustForegroundBrightness(SubjectColor, AdditionalInfoDark, AdditionalInfoLight));
+        
         public float GradeValue => Utilities.GetAverage(_grades, false);
-
-        /// <summary>
-        /// Does the subject's average count for the school-year's average?
-        /// </summary>
+        
         public bool Counts => _counts;
         
-        /// <summary>
-        /// The rounded average of all grades in this subject.
-        /// </summary>
         internal float RoundedAverage => Utilities.GetAverage(_grades, true);
         
-        /// <summary>
-        /// The name of this subject.
-        /// </summary>
         internal string Name => _name;
         
-        /// <summary>
-        /// A collection (list) of all <see cref="Grade">grades</see> in this subject.
-        /// </summary>
         internal List<Grade> Grades => _grades;
-
-        /// <summary>
-        /// Darker tint of the <see cref="SubjectColor"/>
-        /// </summary>
-        private Color DarkSubjectTint => Utilities.DarkenColor(SubjectColor, 0.2f);
         
-        /// <summary>
-        /// Brighter tint of the <see cref="SubjectColor"/>
-        /// </summary>
+        private Color DarkSubjectTint => Utilities.DarkenColor(SubjectColor, 0.2f);
         private Color LightSubjectTint => Utilities.BrightenColor(SubjectColor, 0.2f);
+        
+        private Color AdditionalInfoDark => Utilities.DarkenColor(_additionalInfoColor, 0.3f);
+        private Color AdditionalInfoLight => Utilities.BrightenColor(_additionalInfoColor, 0.3f);
 
         internal void ChangeData(string newName) => ChangeData(newName, _weighting, _counts);
         internal void ChangeData(float newWeighting) => ChangeData(_name, newWeighting, _counts);
         internal void ChangeData(bool counts) => ChangeData(_name, _weighting, counts);
         
-        /// <summary>
-        /// Change the data of this subject.
-        /// </summary>
-        /// <param name="newName">The new name of this subject.</param>
-        /// <param name="newWeighting">The new weighting of the average.</param>
-        /// <param name="counts">Determines whether the subject's average counts for the year's average or not.</param>
         private void ChangeData(string newName, float newWeighting, bool counts)
         {
             _name = newName;
             _weighting = newWeighting;
             _counts = counts;
+        }
+
+        private LinearGradientBrush CreateGradientBrush(RelativePoint startPoint, RelativePoint endPoint, 
+            Color[] colors, double[] offsets)
+        {
+            var gradientStops = new GradientStops();
+            for (int i = 0; i < colors.Length; i++)
+            {
+                gradientStops.Add(new GradientStop(colors[i], offsets[i]));
+            }
+            
+            return new LinearGradientBrush()
+            {
+                StartPoint = startPoint,
+                EndPoint = endPoint,
+                GradientStops = gradientStops
+            };
         }
     }
 }
