1b63592d2cd111ba94ad4f2e0e3ec3d646eac387
Duck <61479436+duck-dev@users.noreply.github.com>
2021-11-09 16:24:32 +0100

    Continue design of grade-list element
    - The grade-list element displays the date and weighting as well. The design and colors are temporary, as well as the image next to the date (it's a placeholder image)
    - Changed some properties inside `Grade.cs` to blend nicely with the View.
    - Changed the order of the properties inside `Grade.cs`.
    - Removed unused constructor of the `Grade` class.

--- src/Models/Grade.cs
+++ src/Models/Grade.cs
@@ -1,27 +1,25 @@
 ﻿using System;
+using System.Globalization;
 using GradeManagement.Interfaces;
-using GradeManagement.UtilityCollection;
 
 namespace GradeManagement.Models
 {
     public class Grade : IGradable
     {
-        public Grade(string name, float value) : this(name, value, 1, DateTime.Today, true) { }
-
         public Grade(string name, float value, float weighting, DateTime date, bool counts)
         {
             this.Name = name;
             this.GradeValue = value;
             this.Weighting = weighting;
-            this.Date = date;
+            this.Date = date.ToString("dd.MM.yyyy", CultureInfo.CurrentCulture); // TODO: Use selected culture
             this.Counts = counts;
         }
         
         public float GradeValue { get; private set; }
-        public float RoundedGrade => (float)Math.Round(GradeValue, 2);
         public float Weighting { get; private set; }
         public bool Counts { get; private set; }
         internal string Name { get; private set; }
-        internal DateTime Date { get; private set; }
+        internal string Date { get; private set; }
+        internal float RoundedGrade => (float)Math.Round(GradeValue, 2);
     }
 }
