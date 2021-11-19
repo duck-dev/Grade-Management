d2c936626d482e03f5300012706435dac94bb485
Duck <61479436+duck-dev@users.noreply.github.com>
2021-11-19 22:31:07 +0100

    Add text to `TargetGradeWindow` buttons
    - Added a label to each of the two buttons in the `TargetGradeWindow`
    - Selecting the other button results in a change of the text color and weight, which strangely doesn't reflect on the UI yet. I will try to fix this issue soon.

--- src/ViewModels/TargetGradeViewModel.cs
+++ src/ViewModels/TargetGradeViewModel.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia.Media;
 using GradeManagement.ViewModels.BaseClasses;
 using ReactiveUI;
@@ -6,10 +7,24 @@ namespace GradeManagement.ViewModels
 {
     public class TargetGradeViewModel : ViewModelBase
     {
+        private const string TargetGradeTitle = "Calculate Target Grade";
+        private const string AverageTitle = "Calculate Average";
+
+        private static readonly SolidColorBrush _whiteColorBrush = new(new Color(255, 255, 255, 255));
+        private static readonly SolidColorBrush _blackColorBrush = new(new Color(255, 0, 0, 0));
+        private string _windowTitle = TargetGradeTitle;
         private int _currentButton;
 
+        private string WindowTitle
+        {
+            get => _windowTitle;
+            set => this.RaiseAndSetIfChanged(ref _windowTitle, value);
+        }
+        
         private static Color GreenColor { get; } = new(255,0,155,114);
         private SolidColorBrush[] ButtonColors { get; } = { new(GreenColor), new(GreenColor,0) };
+        private SolidColorBrush[] ButtonTextColors { get; } = { _whiteColorBrush, _blackColorBrush };
+        private FontWeight[] FontWeights { get; } = { FontWeight.Bold, FontWeight.Normal };
 
         protected override void EraseData()
         {
@@ -22,10 +37,22 @@ namespace GradeManagement.ViewModels
                 return;
             _currentButton = selectedButton;
 
+            var otherButton = Math.Abs(selectedButton - 1);
+
             ButtonColors[selectedButton].Opacity = 1;
-            ButtonColors[selectedButton == 0 ? 1 : 0].Opacity = 0;
+            ButtonColors[otherButton].Opacity = 0;
+
+            ButtonTextColors[selectedButton] = _whiteColorBrush;
+            ButtonTextColors[otherButton] = _blackColorBrush;
+
+            FontWeights[selectedButton] = FontWeight.Bold;
+            FontWeights[otherButton] = FontWeight.Normal;
+
+            WindowTitle = _windowTitle.Equals(AverageTitle) ? TargetGradeTitle : AverageTitle;
 
             this.RaisePropertyChanged(nameof(ButtonColors));
+            this.RaisePropertyChanged(nameof(ButtonTextColors));
+            this.RaisePropertyChanged(nameof(FontWeights));
         }
     }
 }
