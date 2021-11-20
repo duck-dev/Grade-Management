2449c667d622506585e4982d16a59cf4e3127bb9
Duck <61479436+duck-dev@users.noreply.github.com>
2021-11-20 21:24:15 +0100

    Add properties for TargetGrade calculation
    - Added needed properties (and backing fields) for the TargetGrade and AverageGrade calculation.

--- src/ViewModels/TargetGrade/AverageGradeViewModel.cs
+++ src/ViewModels/TargetGrade/AverageGradeViewModel.cs
@@ -1,9 +1,25 @@
 using GradeManagement.ViewModels.BaseClasses;
+using ReactiveUI;
 
 namespace GradeManagement.ViewModels.TargetGrade
 {
     public class AverageGradeViewModel : ViewModelBase
     {
+        private float _grade;
+        private float _weighting;
+        
+        private float Grade
+        {
+            get => _grade;
+            set => this.RaiseAndSetIfChanged(ref _grade, value);
+        }
+
+        private float Weighting
+        {
+            get => _weighting;
+            set => this.RaiseAndSetIfChanged(ref _weighting, value);
+        }
+        
         protected internal override void EraseData()
         {
             // TODO: Erase data
--- src/ViewModels/TargetGrade/TargetGradeViewModel.cs
+++ src/ViewModels/TargetGrade/TargetGradeViewModel.cs
@@ -1,9 +1,25 @@
 using GradeManagement.ViewModels.BaseClasses;
+using ReactiveUI;
 
 namespace GradeManagement.ViewModels.TargetGrade
 {
     public class TargetGradeViewModel : ViewModelBase
     {
+        private float _targetAverage;
+        private float _weighting;
+        
+        private float TargetAverage
+        {
+            get => _targetAverage;
+            set => this.RaiseAndSetIfChanged(ref _targetAverage, value);
+        }
+        
+        private float Weighting
+        {
+            get => _weighting;
+            set => this.RaiseAndSetIfChanged(ref _weighting, value);
+        }
+        
         protected internal override void EraseData()
         {
             // TODO: Erase data
