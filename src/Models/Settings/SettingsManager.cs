5b626230eb35a2d7fce5aa0874aad87c932753dc
Duck <61479436+duck-dev@users.noreply.github.com>
2021-12-19 19:55:14 +0100

    Set SettingsManager up for loading
    - Set `SettingsManager` up for loading settings with specific sub-classes for specific parts of settings (might be reworked later on)

--- src/Models/Settings/SettingsManager.cs
+++ src/Models/Settings/SettingsManager.cs
@@ -1,7 +1,15 @@
+using System;
+
 namespace GradeManagement.Models.Settings
 {
     public static class SettingsManager
     {
-        
+        internal static event EventHandler? LoadingSettings; // Subscribe in static-constructor of a `Settings` class
+
+        internal static void LoadSettings()
+        {
+            LoadingSettings?.Invoke(null, EventArgs.Empty);
+            LoadingSettings = null;
+        }
     }
 }
