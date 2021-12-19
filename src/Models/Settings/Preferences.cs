a96a931c27d31ca450490f8d8b2cfe63c4587754
Duck <61479436+duck-dev@users.noreply.github.com>
2021-12-19 23:22:14 +0100

    Add default button-style (grid)
    - Add default button-style to preferences, being the grid-style.

--- src/Models/Settings/Preferences.cs
+++ src/Models/Settings/Preferences.cs
@@ -1,11 +1,12 @@
 using System;
+using GradeManagement.Views.Lists.ElementButtonControls.Grid;
 
 namespace GradeManagement.Models.Settings
 {
     public class Preferences
     {
-        public Type? YearButtonStyle { get; set; }
-        public Type? SubjectButtonStyle { get; set; }
-        public Type? GradeButtonStyle { get; set; }
+        public Type? YearButtonStyle { get; set; } = typeof(GridYearButton);
+        public Type? SubjectButtonStyle { get; set; } = typeof(GridSubjectButton);
+        public Type? GradeButtonStyle { get; set; } = typeof(GridGradeButton);
     }
 }
