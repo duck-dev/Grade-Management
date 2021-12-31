15c422b76c02525385ef01b5e106ffa2aeb43e9b
Duck <61479436+duck-dev@users.noreply.github.com>
2021-12-31 22:01:31 +0100

    Finally fix view-style panel (for years only in this commit)
    The issue, that the container panel of the items couldn't be switched from `WrapPanel` to `StackPanel` and vice-versa has finally been solved.
    
    - Redesigned the entire structure of quite a big part of YearListView.axaml.
        - Instead of only templating the `ItemsControl.ItemsPanel`, the entire `ItemsControl` is being templated inside the `ScrollView` (which doesn't change at all).
        - The `ItemTemplate` is saved as a resource of the `ContentControl` that's holding that all and can be reused for both templates.
        - Then, there are two DataTemplates of the `ContentControl`, one for `Grid` and the other for `List`. Which one will be used is determined by a `bool` in the DataContext of the UserControl.
    - Added a selector-class (PanelTemplateSelector.cs) with the templates in a dictionary as well as the `Build` and `Match` methods of the implemented `IDataTemplate` interface.

---
+++ src/CustomControls/PanelTemplateSelector.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Avalonia.Metadata;
+
+namespace GradeManagement.CustomControls
+{
+    public class PanelTemplateSelector : IDataTemplate
+    {
+        [Content]
+        public Dictionary<bool, IDataTemplate> Templates { get; } = new();
+        public IControl Build(object data) => Templates[(bool)data].Build(data);
+        public bool Match(object data) => data is bool;
+    }
+}
