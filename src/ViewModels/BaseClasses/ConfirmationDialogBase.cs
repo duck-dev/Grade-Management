653d512c23fc4e39a5de44a9ca978f3e3283363b
Duck <61479436+duck-dev@users.noreply.github.com>
2022-01-12 16:44:00 +0100

    Base structure of ConfirmationDialogViewModel/Base
    - Base structure of ConfirmationDialogViewModel/Base.

--- src/ViewModels/BaseClasses/ConfirmationDialogBase.cs
+++ src/ViewModels/BaseClasses/ConfirmationDialogBase.cs
@@ -1,7 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+using Avalonia.Media;
+
 namespace GradeManagement.ViewModels.BaseClasses
 {
-    public class ConfirmationDialogBase : ViewModelBase
+    public abstract class ConfirmationDialogBase : ViewModelBase
     {
+        protected ConfirmationDialogBase(string title, 
+                                         IEnumerable<SolidColorBrush> borderColors, 
+                                         IEnumerable<SolidColorBrush> buttonTextColors,
+                                         IEnumerable<string> buttonTexts)
+        {
+            this.Title = title;
+            this.BorderColors = borderColors.ToArray();
+            this.ButtonTextColors = buttonTextColors.ToArray();
+            this.ButtonTexts = buttonTexts.ToArray();
+        }
         
+        protected string Title { get; init; }
+        protected SolidColorBrush[] BorderColors { get; init; }
+        protected SolidColorBrush[] ButtonTextColors { get; init; }
+        protected string[] ButtonTexts { get; init; }
     }
 }
--- src/ViewModels/Dialogs/ConfirmationDialogViewModel.cs
+++ src/ViewModels/Dialogs/ConfirmationDialogViewModel.cs
@@ -1,9 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
+using Avalonia.Media;
+using GradeManagement.Interfaces;
 using GradeManagement.ViewModels.BaseClasses;
 
 namespace GradeManagement.ViewModels.Dialogs
 {
     public class ConfirmationDialogViewModel : ConfirmationDialogBase
     {
+        public ConfirmationDialogViewModel(string title,
+            IEnumerable<SolidColorBrush> borderColors,
+            IEnumerable<SolidColorBrush> buttonTextColors,
+            IEnumerable<string> buttonTexts) : base(title, borderColors, buttonTextColors, buttonTexts)
+        { }
+
+        public ConfirmationDialogViewModel(string title,
+            IEnumerable<Color> borderColors,
+            IEnumerable<Color> buttonTextColors,
+            IEnumerable<string> buttonTexts)
+            : this(title, 
+                   borderColors.Select(x => new SolidColorBrush(x)), 
+                   buttonTextColors.Select(x => new SolidColorBrush(x)),
+                   buttonTexts)
+        { }
         
+        private IElement? ElementToRemove { get; init; }
+
+        private void Confirm()
+        {
+            if (ElementToRemove is null)
+                return;
+        }
+
+        private void Cancel()
+        {
+            if (ElementToRemove is null)
+                return;
+        }
+
+        private void CloseDialog()
+        {
+            
+        }
     }
 }
