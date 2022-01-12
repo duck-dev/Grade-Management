d98151d9dac0f52c436ba98fec8bbda5ad15b5b9
Duck <61479436+duck-dev@users.noreply.github.com>
2022-01-12 21:25:28 +0100

    Add `SplitCamelCase` as string extension
    - Added a string extension-method that splits a camelCase (or PascalCase) string into each word and joins them together with a certain separator (default: single space).

---
+++ src/ExtensionCollection/StringExtensions.cs
@@ -0,0 +1,20 @@
+using System.Text.RegularExpressions;
+
+namespace GradeManagement.ExtensionCollection
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Split a string with a camelCase (or PascalCase) pattern into each word and join them with a space together.
+        /// </summary>
+        /// <param name="input">The input string that's supposed to be split.</param>
+        /// <param name="separator">The separator, which will be used to separate each word in the final string.
+        ///                         Default: Single Space.</param>
+        /// <returns>An array of all separate words.</returns>
+        public static string SplitCamelCase(this string input, string separator = " ")
+        {
+            var words = Regex.Split(input, "([A-Z][a-z]+)");
+            return string.Join(separator, words);
+        }
+    }
+}
