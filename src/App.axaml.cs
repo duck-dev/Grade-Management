6b2762a2e311fecdbd8bafb228a3076f08d73831
Duck <61479436+duck-dev@users.noreply.github.com>
2022-02-26 23:15:13 +0100

    Fix changing/setting themes
    - Fixed the (previously broken) behaviour for setting themes:
        - Instead of having a binding and a `StyleInclude` declaration in the XAML-code, it's all being done in the code-behind (App.axaml.cs).

--- src/App.axaml.cs
+++ src/App.axaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Markup.Xaml.Styling;
 using GradeManagement.Enums;
 using GradeManagement.ViewModels;
 using GradeManagement.Views;
@@ -12,35 +13,19 @@ namespace GradeManagement
 {
     public class App : Application
     {
-        private const string ResourcesPath = "/src/Resources/";
-
+        private static readonly string _resourcesPath 
+            = Path.Combine(("avares:" + Path.DirectorySeparatorChar + Path.DirectorySeparatorChar + "GradeManagement"), "src", "Resources");
+        
         private ThemeMode _theme;
+        private Uri? _themeSource;
+        private StyleInclude? _currentThemeStyle;
         private readonly Dictionary<ThemeMode, Uri> _themeSourcesCollection = new()
         {
-            { ThemeMode.Light, new Uri(Path.Combine(ResourcesPath, "LightTheme.axaml")) },
-            { ThemeMode.Dark, new Uri(Path.Combine(ResourcesPath, "DarkTheme.axaml")) },
+            { ThemeMode.Light, new Uri(Path.Combine(_resourcesPath, "LightTheme.axaml")) },
+            { ThemeMode.Dark, new Uri(Path.Combine(_resourcesPath, "DarkTheme.axaml")) }
         };
 
-        private Uri _themeSource;
-        private readonly StyledProperty<Uri> _themeSourceProperty = AvaloniaProperty.Register<App, Uri>(nameof(ThemeSource));
-
-        public App() => _themeSource = ThemeSource = _themeSourcesCollection[ThemeMode.Light]; // TODO: Get from saved settings
-
-        internal ThemeMode Theme
-        {
-            get => _theme;
-            set
-            {
-                _theme = value;
-                ThemeSource = _themeSourcesCollection[_theme];
-            }
-        }
-
-        private Uri ThemeSource
-        {
-            get => _themeSource; 
-            set => this.SetAndRaise(_themeSourceProperty, ref _themeSource, value);
-        }
+        public App() => SetTheme(ThemeMode.Light); // TODO: Get from saved settings
 
         public override void Initialize()
         {
@@ -58,6 +43,20 @@ namespace GradeManagement
             }
 
             base.OnFrameworkInitializationCompleted();
+        }
+
+        internal void SetTheme(ThemeMode theme)
+        {
+            _theme = theme;
+            _themeSource = _themeSourcesCollection[theme];
+
+            if (_currentThemeStyle is not null)
+                this.Styles.Remove(_currentThemeStyle);
+            _currentThemeStyle = new StyleInclude(_themeSourcesCollection[theme])
+            {
+                Source = _themeSourcesCollection[theme]
+            };
+            this.Styles.Add(_currentThemeStyle);
         }
     }
 }
