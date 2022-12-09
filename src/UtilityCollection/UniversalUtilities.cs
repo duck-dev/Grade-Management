69392181e88e0cebdb4affa7571668035761cea1
Duck <61479436+duck-dev@users.noreply.github.com>
2022-12-09 15:40:17 +0100

    Add `Console.WriteLine` to `Log`
    - The `Log` function in the `Utilities` class now also logs the message to the console, besides the debugging trace.

--- src/UtilityCollection/UniversalUtilities.cs
+++ src/UtilityCollection/UniversalUtilities.cs
@@ -45,7 +45,11 @@ namespace GradeManagement.UtilityCollection
         /// Log a message to the console (for debugging purposes).
         /// </summary>
         /// <param name="message">The message to be logged as a string.</param>
-        public static void Log(string? message) => System.Diagnostics.Trace.WriteLine(message);
+        public static void Log(string? message)
+        {
+            Console.WriteLine(message);
+            System.Diagnostics.Trace.WriteLine(message);
+        }
 
         /// <summary>
         /// Retrieves a resource from a specified <see cref="IResourceNode"/> and tries to cast it to the specified type.
