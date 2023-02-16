51df44749cf86a8fa3d8db0a8f7426a4fc35ef08
Duck <61479436+duck-dev@users.noreply.github.com>
2023-02-16 16:49:47 +0100

    Add extension methods for (nullable) float comparison
    - Added two extension methods to FloatExtensions.cs:
        - `AlmostEquals(float?, float?, float)`: Compares two nullable floats for equality with a given tolerance.
        - `AlmostEquals(float, float, float`: Compares two floats for equality with a given tolerance.

--- src/ExtensionCollection/FloatExtensions.cs
+++ src/ExtensionCollection/FloatExtensions.cs
@@ -16,5 +16,31 @@ namespace GradeManagement.ExtensionCollection
             amount = Math.Clamp(amount, 0, 1);
             return start + (end - start) * amount;
         }
+
+        /// <summary>
+        /// Test equality between two nullable floats.
+        /// </summary>
+        /// <param name="a">Nullable float A</param>
+        /// <param name="b">Nullable float B</param>
+        /// <param name="tolerance">The tolerance for deviation.</param>
+        /// <returns>Returns `true` if the absolute difference is smaller than the tolerance, otherwise it returns `false`.</returns>
+        public static bool AlmostEquals(this float? a, float? b, float tolerance)
+        {
+            // ReSharper disable once CompareOfFloatsByEqualityOperator
+            if (a is not { } aFloat || b is not { } bFloat)
+                return a == b;
+
+            return AlmostEquals(aFloat, bFloat, tolerance);
+        }
+        
+        /// <summary>
+        /// Test equality between two floats.
+        /// </summary>
+        /// <param name="a">Float a</param>
+        /// <param name="b">Float b</param>
+        /// <param name="tolerance">The tolerance for deviation.</param>
+        /// <returns>Returns `true` if the absolute difference is smaller than the tolerance, otherwise it returns `false`.</returns>
+        public static bool AlmostEquals(this float a, float b, float tolerance)
+            => Math.Abs(a - b) < tolerance;
     }
 }
