ce8f1740b48857b8065f3685b055210e91a5d34c
Duck <61479436+duck-dev@users.noreply.github.com>
2023-02-23 15:04:37 +0100

    [VERY UNSTABLE] Setup base for drag-and-drop for elements
    - Setup the base for the drag-and-drop feature for elements. This functionality is very unstable and doesn't work yet. Elements can be dragged (worked in previous commits as well), but the data transfer doesn't work and there are no effects implemented yet.

--- src/Views/CustomControls/CustomDragPanel.axaml.cs
+++ src/Views/CustomControls/CustomDragPanel.axaml.cs
@@ -8,19 +8,23 @@ using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 using Avalonia.Styling;
 using Avalonia.VisualTree;
+using GradeManagement.CustomDragDrop;
+using GradeManagement.Models.Elements;
 using GradeManagement.ViewModels;
 
 namespace GradeManagement.Views.CustomControls;
 
-public class CustomDragPanel : Panel, IStyleable
+public class CustomDragPanel : Panel, IStyleable, IDragObject, IDropTarget
 {
-    private const double MinDragOffset = 10;
+    private const double MinDragOffset = 10; // DragOBJECT
     
-    private bool _isPressed;
-    private Point _previousPosition;
-    private Point? _initialPosition;
-    private TranslateTransform? _transform;
+    private bool _isPressed; // DragOBJECT
+    private Point _previousPosition; // DragOBJECT
+    private Point? _initialPosition; // DragOBJECT
+    private TranslateTransform? _transform; // DragOBJECT
     private ContentPresenter? _visualParent; // Set ZIndex of ContentPresenter, otherwise it will have no effect
+    private Element? _element;
+    private IDropTarget? _dropTarget; // DragOBJECT
 
     public CustomDragPanel()
     {
@@ -30,19 +34,52 @@ public class CustomDragPanel : Panel, IStyleable
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
-        // The following actions are necessary for recognizing left-click as well
+        
+        // The following handlers are necessary for recognizing left-click as well:
+        // DragOBJECT
         AddHandler(PointerPressedEvent, OnPointerPressed, RoutingStrategies.Tunnel, true);
         AddHandler(PointerMovedEvent, OnPointerMoved, RoutingStrategies.Tunnel, true);
         AddHandler(PointerReleasedEvent, OnPointerReleased, RoutingStrategies.Tunnel, true);
+
         this.Initialized += (sender, args) =>
         {
             _initialPosition = Bounds.Position;
             _visualParent = this.GetVisualParent<ContentPresenter>();
+            if (this.DataContext is Element element)
+                _element = element;
         };
     }
 
     Type IStyleable.StyleKey => typeof(Panel);
+    
+    // DropTARGET
+    private bool IsDragDropValid
+        => DragDropService.DataObject is { } dataObject && dataObject.Data is Element element && !ReferenceEquals(_element, element);
+    
+    // DragOBJECT
+    public void OnDragOverTarget(IDropTarget target)
+    {
+        _dropTarget = target;
+    }
 
+    // DragOBJECT
+    public void OnDragLeaveTarget(IDropTarget target)
+    {
+        if (ReferenceEquals(target, _dropTarget))
+            _dropTarget = null;
+        else
+            throw new ArgumentException("The drag target doesn't match the currently assigned drag target.", nameof(target));
+    }
+
+    // DropTARGET
+    public void DropData(object data)
+    {
+        if (data is not Element element)
+            return;
+        // TODO: Handle dropping data
+    }
+
+    // DragOBJECT
     private void OnPointerPressed(object? sender, PointerPressedEventArgs e)
     {
         // Only proceed if the left mouse button was pressed
@@ -52,15 +89,23 @@ public class CustomDragPanel : Panel, IStyleable
         Point currentPos = e.GetPosition(Parent);
         _isPressed = true;
         MainWindowViewModel.IsElementDragged = false;
+        
         if(_visualParent != null)
             _visualParent.ZIndex = int.MaxValue;
+
         _previousPosition = currentPos;
         if (_transform != null)
             _previousPosition = new Point(_previousPosition.X - _transform.X, _previousPosition.Y - _transform.Y);
         
+        if (_element is null)
+            return;
+        var dataObject = new DragDataObject(_element, this);
+        DragDropService.StartDragDrop(dataObject);
+        
         base.OnPointerPressed(e);
     }
     
+    // DragOBJECT
     private void OnPointerMoved(object? sender, PointerEventArgs e)
     {
         // Only proceed if the left mouse button was pressed
@@ -77,13 +122,45 @@ public class CustomDragPanel : Panel, IStyleable
         base.OnPointerMoved(e);
     }
     
+    // DragOBJECT
     private void OnPointerReleased(object? sender, PointerReleasedEventArgs e)
     {
         _isPressed = false;
         if(_visualParent != null)
             _visualParent.ZIndex = 0;
         SetTransform(_initialPosition!.Value.X, _initialPosition.Value.Y); // Reset position
+
+        if (DragDropService.DataObject is { } dataObject && ReferenceEquals(dataObject.DragObject, this) && ReferenceEquals(dataObject.Data, _element))
+            _dropTarget?.DropData(dataObject.Data);
+        DragDropService.StopDragDrop();
+        // TODO: Drop action related to DragObject: either REORDER or MOVE (delete from parent, move to grade group)
+        
         base.OnPointerReleased(e);
+    }
+
+    // DropTARGET
+    protected override void OnPointerEnter(PointerEventArgs e)
+    {
+        if (!IsDragDropValid)
+            return;
+        DragDropService.DataObject!.DragObject.OnDragOverTarget(this);
+        // TODO: Setup visual drag over effects (this is the drop TARGET)
+        base.OnPointerEnter(e);
+    }
+
+    // DropTARGET
+    protected override void OnPointerLeave(PointerEventArgs e)
+    {
+        if (IsDragDropValid)
+            DragDropService.DataObject!.DragObject.OnDragLeaveTarget(this);
+        ResetDragOverEffects();
+        base.OnPointerLeave(e);
+    }
+
+    // DropTARGET
+    private void ResetDragOverEffects()
+    {
+        // TODO: Reset visual drag over effects
     }
 
     private void SetTransform(double offsetX, double offsetY)
